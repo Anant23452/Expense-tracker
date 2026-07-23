1c56d6aba8ee785d576be47963342c65604788af
anantkpatel <imagine231216@gmail.com>
2026-07-23 15:24:27 +0530

    oi

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -2,7 +2,7 @@ import React, { useState, type ChangeEvent } from 'react'
 
 export default function ExpenseForm() {
   const [title,setTitle]=useState("");
-  const [amount,setAmount]=useState();
+  const [amount,setAmount]=useState("");
   const [category,setCategory]=useState("")
   const [date,setDate] =useState("");
   const handleTitleEvent=((
@@ -15,14 +15,24 @@ export default function ExpenseForm() {
    <div className="main flex flex-col justify-center gap-1  items-center 
    ">
      <div>ExpenseForm</div>
-    <input 
 
+     {/* Title input value  */}
+    <input 
     type="text" 
     placeholder='type expenses'
     value={title}
     onChange={handleTitleEvent}
     className='border-2 rounded p-2'
 
+    />
+
+    {/* //Amount input value  */}
+    <input
+    type ="number"
+    placeholder='Enter Amount'
+    value={amount}
+    onChange={(e)=>setAmount(e.target.value)}
+    
     />
     <p>{title} </p>
    </div>
