b71b991c88e922806770dea3c54e0ac87edf4f4b
anantkpatel <imagine231216@gmail.com>
2026-07-23 14:46:20 +0530

    lkj

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,14 +4,20 @@ export default function ExpenseForm() {
   const [title,setTitle]=useState("");
   return (
     <>
-    <div>ExpenseForm</div>
+   <div className="main flex flex-col justify-center gap-1  items-center 
+   ">
+     <div>ExpenseForm</div>
     <input 
+
     type="text" 
     placeholder='type expenses'
     value={title}
     onChange={(e)=>setTitle(e.target.value)}
+    className='border-2 rounded p-2'
 
     />
+    <p>{title} </p>
+   </div>
     </>
   )
 }
