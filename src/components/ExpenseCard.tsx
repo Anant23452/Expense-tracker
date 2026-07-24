d60a04eb7245a7769d950ed5afb8b9da15f6b329
anantkpatel <imagine231216@gmail.com>
2026-07-24 14:26:06 +0530

    uy

--- src/components/ExpenseCard.tsx
+++ src/components/ExpenseCard.tsx
@@ -7,7 +7,7 @@ type ExpnseFormProps={
         <>
         {/* making ui for expenses  */}
         <div className="border rounded-lg  p-4 shadow-md mb-3">
-            
+            <h3>{expense.title} </h3>
         </div>
 
         
