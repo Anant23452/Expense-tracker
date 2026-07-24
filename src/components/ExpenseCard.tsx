a336276a838013fb2929496bab4d091e0a978180
anantkpatel <imagine231216@gmail.com>
2026-07-24 13:53:28 +0530

    ana

---
+++ src/components/ExpenseCard.tsx
@@ -0,0 +1,13 @@
+import { Expense } from "../types/expense";
+type ExpnseFormProps={
+    expense:Expense
+}
+ export function ExpenseCard({expense}:ExpnseFormProps){
+    return(
+        <>
+        {/* making ui for expenses  */}
+        
+        
+        </>
+    )
+}
