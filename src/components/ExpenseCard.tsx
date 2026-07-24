9ddb2d8f50989140407d531cd0a545a4274dc5b5
anantkpatel <imagine231216@gmail.com>
2026-07-24 16:22:18 +0530

    i

--- src/components/ExpenseCard.tsx
+++ src/components/ExpenseCard.tsx
@@ -7,7 +7,7 @@ type ExpenseCardProps={
     return(
         <>
         {/* making ui for expenses  */}
-        <div className="border rounded-lg  p-4 shadow-md mb-3">
+        <div className="border rounded-lg  p-4 shadow-md mb-3 bg-blue-300">
             <h3>{expense.title} </h3>
             <p>Rs{expense.amount} </p>
             <p>{expense.category} </p>
