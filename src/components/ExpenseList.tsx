11149a001c767c65a35be40e324289f8ff30ed9d
anantkpatel <imagine231216@gmail.com>
2026-07-25 16:03:12 +0530

    y

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -11,7 +11,10 @@ function ExpenseList({ expenses,deleteExpense }: ExpenseListProps) {
   return (
    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-4xl mx-auto p-4 bg-blue-300">
       {expenses.map((expense) => (
-        <ExpenseCard key={expense.id} expense={expense} />
+        <ExpenseCard
+         key={expense.id} 
+         expense={expense}
+          />
       ))}
     </div>
   );
